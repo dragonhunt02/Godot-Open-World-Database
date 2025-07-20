0439b3643b4578d7df4f65e91da2169098448afe
Luke Heyburn <luke@devanew.com>
2025-07-20 23:08:49 +0100

    updated icon

--- addons/open-world-database/plugin.gd
+++ addons/open-world-database/plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("OpenWorldDatabase", "Node", preload("src/open_world_database.gd"), preload("icon.svg"))
+	add_custom_type("OpenWorldDatabase", "Node", preload("src/open_world_database.gd"), preload("icon.png"))
 
 func _exit_tree():
 	remove_custom_type("OpenWorldDatabase")
