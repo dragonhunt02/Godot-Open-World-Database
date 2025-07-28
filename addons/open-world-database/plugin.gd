7bbc1845fb93dfe439e5d21cb1ecdf7bde83e630
Luke Heyburn <luke@devanew.com>
2025-07-28 02:10:10 +0100

    updated readme and added new svg logo

--- addons/open-world-database/plugin.gd
+++ addons/open-world-database/plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-	add_custom_type("OpenWorldDatabase", "Node", preload("src/open_world_database.gd"), preload("icon.png"))
+	add_custom_type("OpenWorldDatabase", "Node", preload("src/open_world_database.gd"), preload("icon.svg"))
 
 func _exit_tree():
 	remove_custom_type("OpenWorldDatabase")
